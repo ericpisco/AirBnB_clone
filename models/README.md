3b575753adf36231aa15198221119978cc7efa3b
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-05-14 01:52:51 -0700

    models package initialization

---
+++ models/README.md
@@ -0,0 +1,1 @@
+a class BaseModel that defines all common attributes/methods for other classes
---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#models package initialization
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
